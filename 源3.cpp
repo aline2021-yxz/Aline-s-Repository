a893b0fd9d1c8965e72ece046ea01abfd2badc95
月落惜伤时 <1445992769@qq.com>
2021-03-14 23:40:20 +0800

    上传以展示

---
+++ 源3.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	char arr1[10];
+//	char arr2[10];
+//	printf("请输入要连接的两组字符\n");
+//	printf("第一组:\n");
+//	gets(arr1);
+//	printf("第二组:\n");
+//	gets(arr2);
+//    strcat (arr1, arr2);
+//	printf("连接后的字符串是:\n");
+//	puts(arr1);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int max = 0;
+//	char arr1[10], arr2[10], arr3[10];
+//	gets(arr1);
+//	gets(arr2);
+//	gets(arr3);
+//	max = sizeof(arr1);
+//	if (arr1<arr2&&arr1>arr3)
+//	{
+//		printf("arr2\n");
+//		printf("arr1\n");
+//		printf("arr3\n");
+//
+//	}
+//
+//	return 0;
+////}
+//int main()
+//{
+//	int a = 0;
+//	int arr1[10];
+//	scanf("%s",arr1);
+//	printf("%s ", arr1);
+//	int c = 0;
+//	scanf("%d",&c);
+//	for (a = 9-c; a <= 10; a++)
+//	{
+//		int b = 0;
+//		for (b = 0; b < 10 - c; c++)
+//		{
+//			int tmp = arr1[a];
+//			arr1[a] = arr1[a - 1];
+//			arr1[a - 1] = tmp;
+//		}
+//	}
+//	printf("%s ", arr1);
+//	return 0;
+//}
