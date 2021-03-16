39d137a2c32915c0d173340c825fa5656fa25c81
月落惜伤时 <1445992769@qq.com>
2021-03-17 00:02:42 +0800

    上传github以展示

---
+++ 源4.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//int main()
+//{
+//	float a ;
+//	int b;
+//	a= b= 1.25;
+//
+//	printf("%d\n", b);
+//	printf("%lf\n", a);
+// 	return 0;
+//}
+//int main()
+//{
+//	char arr1[3] = { 0 };
+//	char arr2[3] = { 0 };
+//	char arr3[3] = { 0 };
+//	gets(arr1);
+//	gets(arr2);
+//	gets(arr3);
+//	
+//
+//	return 0;
+////}
+//void change(int arr[10000],int x)
+//{
+//	int a = 0;
+//	int b = 0;
+//	for (a = 2; a < x; a++)
+//	{
+//		/*if (a = 2)
+//		{
+//			arr[2] = 0;
+//		}*/
+//		int tmp = arr[a+1];
+//		arr[a + 1] = arr[a];
+//		arr[a ] = tmp;
+//	}
+//}
+//int main()
+//{
+//    int c = 0;
+//	int a = 0;
+//	int x = 0;
+//	scanf("%d", &a);
+//	int arr[10000] = { 0 };
+//	for (int b = 0; b < a; b++)
+//	{
+//		arr[x] = b;
+//		x++;
+//	}
+//	change(arr,x);
+//	for (c = 0; c < x; c++)
+//	{
+//		printf("%d ", arr[c]);
+//	}
+//	return 0;
+//}
