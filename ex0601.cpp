a207798bc0a66712110a28293734cd1798cbe519
月落惜伤时 <1445992769@qq.com>
2021-04-12 22:32:46 +0800

    上传展示

---
+++ ex0601.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include "ex0601.h"
+using namespace std;
+
+//int main()
+//{
+//	int x, y;
+//	cin >> x >> y;
+//	cout << MY_pow(x,y) << endl;
+//	return 0;
+//}
+
+
+
+//
+//#define SQUARE(x)  (x)*(x)
+//#define DIST(x1,y1,x2,y2) sqrt((double)((SQUARE(x1-x2))+(SQUARE(y1-y2))))
+//
+//int main()
+//{
+//	int x1, y1, x2, y2;
+//	cout << "请输入第1个点的坐标>";
+//	cin >> x1 >> y1;
+//	cout << endl;
+//	cout << "请输入第2个点的坐标>";
+//	cin >> x2 >> y2;
+//	cout << DIST(x1, y1, x2, y2) << endl;
+//
+//	return 0;
+//}
+//
+
+
+
+//double square(int x)
+//{
+//	return (x)*(x);
+//}
+//double dist(int x1, int y1, int x2, int y2)
+//{
+//	return sqrt((double)square(x1 - x2) + square(y1 - y2));
+//}
+//
+//int main()
+//{
+//		int x1, y1, x2, y2;
+//		cout << "请输入第1个点的坐标>";
+//		cin >> x1 >> y1;
+//		cout << endl;
+//		cout << "请输入第2个点的坐标>";
+//		cin >> x2 >> y2;
+//		cout << dist(x1, y1, x2, y2) << endl;
+//	
+//	return 0;
+//}
+
+
+//#define MIN2(x,y)   ((x)<(y)?(x):(y))
+//#define MIN4(w,x,y,z)	 (MIN2((z),(MIN2((x),MIN2(w,y)))))
+//
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	cout << MIN4(a, b, c, d)<<endl;
+//	return 0;
+//}
